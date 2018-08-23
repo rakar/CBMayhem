0aeefdfaad2ebf0feff25a995a3ab723879773e6
rakar <rich.kopelow@gmail.com>
2018-08-23 01:03:15 -0400

    Add operator behavior to handle shoot cube button, which was mapped but ignored.

---
+++ src/main/java/frc/team555/Mayhem/Behaviors/OperatorBehavior.java
@@ -0,0 +1,39 @@
+package frc.team555.Mayhem.Behaviors;
+
+import frc.team555.Mayhem.Data.ControlData;
+import frc.team555.Mayhem.Data.RequestData;
+import org.montclairrobotics.cyborg.Cyborg;
+import org.montclairrobotics.cyborg.behaviors.CBBehavior;
+import org.montclairrobotics.cyborg.data.CBStdDriveRequestData;
+
+public class OperatorBehavior  extends CBBehavior {
+    private RequestData rd;
+    private ControlData cd;
+    private CBStdDriveRequestData intake;
+
+    public OperatorBehavior(Cyborg robot) {
+        super(robot);
+
+        rd = (RequestData) Cyborg.requestData;
+        cd = (ControlData) Cyborg.controlData;
+        intake = rd.intake;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+
+        if(rd.shootCube) {
+            intake.active=true;
+            intake.direction.setXY(0,1); // bad value - shoot cube?
+            intake.rotation = 0;
+        } else {
+            if(!intake.active) {
+                intake.active = true;
+                intake.direction.setXY(0, -.20); // bad value - Draw cube in?
+                intake.rotation = 0;
+            }
+        }
+    }
+
+}
